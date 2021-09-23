f81f38e9a150a3a9fd6efba7e0278bdb451bf93a
Emilia <emiliastarczyk@gmail.com>
2021-09-23 15:35:32 +0200

    Implemented ValueChangedEvent for defining value of color for ValuePicker

--- VectorMaker/ControlsResources/ColorPicker.xaml.cs
+++ VectorMaker/ControlsResources/ColorPicker.xaml.cs
@@ -1,17 +1,23 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using WindowsColor = System.Windows.Media.Color;
+using WindowsMedia = System.Windows.Media;
 
 namespace VectorMaker.ControlsResources
 {
     /// <summary>
     /// Interaction logic for ColorPicker.xaml
     /// </summary>
-    public partial class ColorPicker : UserControl
+    public partial class ColorPicker : UserControl, INotifyPropertyChanged
     {
         private WindowsColor m_pickedColor;
         private Color m_pickedColorClass;
         private WindowsColor m_baseColor;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public WindowsColor PickedColor
         {
@@ -22,7 +28,11 @@ namespace VectorMaker.ControlsResources
         public WindowsColor BaseColor
         {
             get { return m_baseColor; }
-            set { m_baseColor = value; }
+            set
+            {
+                m_baseColor = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BaseColor"));
+            }
         }
 
         public ColorPicker()
@@ -36,13 +46,13 @@ namespace VectorMaker.ControlsResources
         private void SetBaseColor(WindowsColor color)
         {
             BaseColor = color;
+            Picker.InvalidateVisual();
         }
 
         private void CalculateBaseColorOnPickedColor()
         {
             Color tempColor = new Color(m_pickedColorClass.HSLColor.H.IntProperty, 100, 50, 255, true);
-            BaseColor = tempColor.ColorInWindowsFormat;
-            Picker.InvalidateVisual();
+            SetBaseColor(tempColor.ColorInWindowsFormat);
         }
 
         private void SetCursor(System.Drawing.Point position)
@@ -50,5 +60,37 @@ namespace VectorMaker.ControlsResources
 
         }
 
+        private void ValuePicker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            WindowsColor color = GetColorFromValueGradient(e.NewValue);
+            SetBaseColor(color);
+
+        }
+
+        private static WindowsColor GetColorFromValueGradient(double offset)
+        {
+            WindowsColor tempColor = new WindowsColor();
+            WindowsMedia.GradientStop gradientStopBeforeOffsetThreshold;
+            WindowsMedia.GradientStop gradientStopAfterOffsetThreshold = 
+                ColorsReference.valueGradientStopListSegregated.FirstOrDefault(x => x.Offset >= offset);
+
+            int index = ColorsReference.valueGradientStopListSegregated.IndexOf(gradientStopAfterOffsetThreshold);
+            if (index == 0)
+            {
+                gradientStopBeforeOffsetThreshold = gradientStopAfterOffsetThreshold;
+                tempColor = gradientStopBeforeOffsetThreshold.Color;
+                return tempColor;
+            }
+            else
+                gradientStopBeforeOffsetThreshold = ColorsReference.valueGradientStopListSegregated[index - 1];
+
+            tempColor.ScA = (float)((offset - gradientStopBeforeOffsetThreshold.Offset) * (gradientStopAfterOffsetThreshold.Color.ScA - gradientStopBeforeOffsetThreshold.Color.ScA) / (gradientStopAfterOffsetThreshold.Offset - gradientStopBeforeOffsetThreshold.Offset) + gradientStopBeforeOffsetThreshold.Color.ScA);
+            tempColor.ScR = (float)((offset - gradientStopBeforeOffsetThreshold.Offset) * (gradientStopAfterOffsetThreshold.Color.ScR - gradientStopBeforeOffsetThreshold.Color.ScR) / (gradientStopAfterOffsetThreshold.Offset - gradientStopBeforeOffsetThreshold.Offset) + gradientStopBeforeOffsetThreshold.Color.ScR);
+            tempColor.ScG = (float)((offset - gradientStopBeforeOffsetThreshold.Offset) * (gradientStopAfterOffsetThreshold.Color.ScG - gradientStopBeforeOffsetThreshold.Color.ScG) / (gradientStopAfterOffsetThreshold.Offset - gradientStopBeforeOffsetThreshold.Offset) + gradientStopBeforeOffsetThreshold.Color.ScG);
+            tempColor.ScB = (float)((offset - gradientStopBeforeOffsetThreshold.Offset) * (gradientStopAfterOffsetThreshold.Color.ScB - gradientStopBeforeOffsetThreshold.Color.ScB) / (gradientStopAfterOffsetThreshold.Offset - gradientStopBeforeOffsetThreshold.Offset) + gradientStopBeforeOffsetThreshold.Color.ScB);
+
+            return tempColor;
+        }
     }
 }
--- VectorMaker/ControlsResources/ColorsReference.cs
+++ VectorMaker/ControlsResources/ColorsReference.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Media;
 
 namespace VectorMaker { 
@@ -8,5 +10,9 @@ namespace VectorMaker {
         public static SolidColorBrush notSelectedTabItemBackground = (SolidColorBrush)Application.Current.FindResource("TabItemBackgroundNotSelectedColor");
         public static System.Windows.Media.Color magentaBaseColor = (System.Windows.Media.Color)Application.Current.FindResource("MagentaBaseColor");
         public static SolidColorBrush magentaBaseBrush = (SolidColorBrush)Application.Current.FindResource("MagentaBaseBrush");
+        public static GradientBrush valueGradientBrush = (GradientBrush)Application.Current.FindResource("ValueGradient");
+        public static GradientStopCollection valueGradientStopCollection = valueGradientBrush.GradientStops;
+        public static List<GradientStop> valueGradientStopListSegregated = valueGradientBrush.GradientStops
+            .OrderBy(x=>x.Offset).ToList();
     }
 }
