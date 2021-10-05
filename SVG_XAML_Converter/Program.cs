f02edb1282e1d7c55478cdacb218da95638b018b
Emilia Starczyk <emiliastarczyk@gmail.com>
2021-10-05 14:55:46 +0200

    Added new project - SVG_XAML_converter, added System.Xaml.dll

---
+++ SVG_XAML_Converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SVG_XAML_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SVG_XAML_Converter/SVG_To_XAML.cs
@@ -0,0 +1,11 @@
+﻿using System.Xaml;
+namespace SVG_XAML_Converter
+{
+    static class SVG_To_XAML
+    {
+        public static System.Xaml.XamlNodeList ConvertSVGToXamlCode()
+        {
+            return null;
+        }
+    }
+}
