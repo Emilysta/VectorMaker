d3aa31f2c441f8cad41d7f2f43578a3cba951c37
Emilia <emiliastarczyk@gmail.com>
2021-10-06 13:28:18 +0200

    Deleted using system.xaml, added system.xml -> XmlDocument

--- SVG_XAML_Converter/SVG_To_XAML.cs
+++ SVG_XAML_Converter/SVG_To_XAML.cs
@@ -1,11 +1,22 @@
-﻿using System.Xaml;
+﻿using System.Xml;
 namespace SVG_XAML_Converter
 {
     static class SVG_To_XAML
     {
-        public static System.Xaml.XamlNodeList ConvertSVGToXamlCode()
+        public static void ConvertSVGToXamlCode()
         {
-            return null;
+            
+        }
+
+        private static void LoadSVGFile(string fileName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            try { doc.Load(fileName); } //check if .svg can be readed
+            catch (System.IO.FileNotFoundException)
+            {
+                //to do
+            }
         }
     }
 }
