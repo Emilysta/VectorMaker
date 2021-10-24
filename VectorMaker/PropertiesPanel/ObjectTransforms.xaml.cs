be1f0f76e7fc6ca5dc750a84f3598fc2d75d0f64
Emilia Starczyk <emiliastarczyk@gmail.com>
2021-10-24 19:01:57 +0200

    ObjectTransforms fully in Docking

--- VectorMaker/PropertiesPanel/ObjectTransforms.xaml.cs
+++ VectorMaker/PropertiesPanel/ObjectTransforms.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using VectorMaker.Pages;
 
 namespace VectorMaker.PropertiesPanel
@@ -10,6 +12,7 @@ namespace VectorMaker.PropertiesPanel
     /// </summary>
     public partial class ObjectTransforms : UserControl
     {
+        private Frame frame;
         private bool IsOneObjectSelected = false;
         public ObjectTransforms()
         {
@@ -20,7 +23,24 @@ namespace VectorMaker.PropertiesPanel
         {
             if(IsOneObjectSelected)
             {
-               
+                Transform transform = (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderTransform;
+                TranslateTransform translateTransform = new TranslateTransform();
+                translateTransform.X = (double)TranslationXTextBox.Value;
+                translateTransform.Y = (double)TranslationYTextBox.Value;
+                TransformGroup transformGroup = transform as TransformGroup;
+                if (transformGroup == null)
+                {
+                    transformGroup = new TransformGroup();
+                    transformGroup.Children.Add(transform);
+                    transformGroup.Children.Add(translateTransform);
+                    (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderTransform = transformGroup;
+                }
+                else
+                {
+                    transformGroup.Children.Add(translateTransform);
+                }
+                
+                (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.InvalidateVisual();
             }
         }
 
@@ -28,7 +48,31 @@ namespace VectorMaker.PropertiesPanel
         {
             if (IsOneObjectSelected)
             {
+                Transform transform = (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderTransform;
+                RotateTransform rotateTransform = new RotateTransform();
+                //rotateTransform.CenterX = (double)RotateCenterXBox.Value;
+                //rotateTransform.CenterY = (double)RotateCenterYBox.Value;
+                Point toTranslate = new Point((frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderSize.Width / 2,
+                    (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderSize.Height / 2);
+                Point center = (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.TranslatePoint(toTranslate, (frame.Content as DrawingCanvas).MainCanvas);
+                rotateTransform.Angle = (double)RotateAngleBox.Value;
+                rotateTransform.CenterX = center.X;
+                rotateTransform.CenterY = center.Y;
 
+                TransformGroup transformGroup = transform as TransformGroup;
+                if (transformGroup == null)
+                {
+                    transformGroup = new TransformGroup();
+                    transformGroup.Children.Add(transform);
+                    transformGroup.Children.Add(rotateTransform);
+                    (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.RenderTransform = transformGroup;
+                }
+                else
+                {
+                    transformGroup.Children.Add(rotateTransform);
+                }
+
+                (frame.Content as DrawingCanvas).SelectedObjects[0].AdornedElement.InvalidateVisual();
             }
         }
 
@@ -50,8 +94,8 @@ namespace VectorMaker.PropertiesPanel
 
         private bool CheckSelection()
         {
-            Frame frame = MainWindow.Instance.DockingManager.ActiveContent as Frame;
-           // MainWindow.Instance.DockingManager.Con
+            frame = MainWindow.Instance.DockingManager.ActiveContent as Frame;
+            // MainWindow.Instance.DockingManager.Con
             if ((frame.Content as DrawingCanvas).SelectedObjects.Count > 1)
             {
                 BlockingBlend.Visibility = Visibility.Visible;
